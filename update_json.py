9ce8a2bdc17ca5f1cc911e1679794b5528117a39
Andrew <Andrewfanggara21@gmail.com>
2019-10-26 20:30:52 +0800

    Add

--- update_json.py
+++ update_json.py
@@ -2,32 +2,36 @@ import json
 import datetime 
 
 def generate_json(data):
-    with open('car.json', 'w') as outfile:
+    with open('car.json', 'a') as outfile:
         outfile.write(json.dumps(data, indent=4, sort_keys=True, default=str))
     outfile.close()
 
 def is_crashed(Car):
     time = datetime.datetime.now()
     data = {
-        "user_name": Car.user,
-        "police_number": Car.police_number,
-        "airbag_open": True,
-        "is_squiggly": False,
-        "time": time,
-        "x_coordinate": Car.x,
-        "y_coordinate": Car.y
+        "cars": [{
+            "user_name": Car.user,
+            "police_number": Car.police_number,
+            "airbag_open": True,
+            "is_squiggly": False,
+            "time": time,
+            "x_coordinate": Car.x,
+            "y_coordinate": Car.y
+        }]
     }
     generate_json(data)
 
 def is_squiggly(Car):
     time = datetime.datetime.now()
     data = {
-        "user_name": Car.user,
-        "police_number": Car.police_number,
-        "airbag_open": False,
-        "is_squiggly": True,
-        "time": time,
-        "x_coordinate":Car.x,
-        "y_coordinate": Car.y
+        "cars": [{
+            "user_name": Car.user,
+            "police_number": Car.police_number,
+            "airbag_open": False,
+            "is_squiggly": True,
+            "time": time,
+            "x_coordinate":Car.x,
+            "y_coordinate": Car.y
+        }]
     }
     generate_json(data)
